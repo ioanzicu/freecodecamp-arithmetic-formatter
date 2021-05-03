96e82483308379d220335c79df7ea8932b238ae6
Ioan <ioan.zicu94@gmail.com>
2021-05-03 14:51:52 +0200

    refactor rjust to f-string; extract logic in separate functions

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,19 +1,80 @@
+def try_parse_number_int(str_number):
+    '''
+    Try to parse the string to an integer.
+            Successful parsing   - it returns an integer value
+            Unsuccessful parsing - return None value
+    '''
+
+    try:
+        number = int(str_number)
+        return number
+    except:
+        return None
+
+
+def format_result_str(item_list, new_line=False) -> str:
+    '''
+    Format the items in the `item_list` by adding 4 white spaces.
+    It does not add extra 4 white spaces before the first item.
+            Returns a string
+    '''
+
+    first = True
+    result_str = ''
+    for item in item_list:
+        if first:
+            result_str += item
+            first = False
+        else:
+            result_str += f'    {item}'
+
+    if new_line:
+        result_str += '\n'
+
+    return result_str
+
+
+def add_or_substruct_int(operand1, operator, operand2) -> int:
+    '''
+    It performs addition or subtraction arithmetic operation.
+            Return an integer result
+    '''
+
+    result = 0
+    if operator == '+':
+        result = operand1 + operand2
+    if operator == '-':
+        result = operand1 - operand2
+
+    return result
+
+
 def arithmetic_arranger(args, display_answer=False):
     '''
-    Expected input: ['1 + 1', '4 - 2']
-        - A single space between the operator
-        - Only two operands
-        - Accepted operands: +, -
+    Expected input:
+        First Parameter: ['1 + 1', '4 - 2']
+            - A single space between the operator
+            - Only two operands
+            - Accepted operands: +, -
 
+        Second Parameter: `True` / `False`
 
     Example of call:
-    arithmetic_arranger(['1 + 1', '4 - 2'])
+        arithmetic_arranger(['1 + 1', '4 - 2'])
 
-    Result format:
-        1
-      + 1
-      ---
-        2
+        Result format:
+          1
+        + 1
+        ---
+
+    Example of call with the flag `display_answer=True` 
+        arithmetic_arranger(['1 + 1', '4 - 2'], display_answer=True)
+
+        Result format:
+          1
+        + 1
+        ---
+          2
     '''
 
     args_len = len(args)
@@ -38,9 +99,7 @@ def arithmetic_arranger(args, display_answer=False):
         if operator not in accepted_operators:
             return 'Error: Operator must be \'+\' or \'-\'.'
 
-        # @TODO Negate
-        if (operand1 := try_parse_number(operand1)) and (operand2 := try_parse_number(operand2)):
-            # @TODO Extract in a separate function
+        if (operand1 := try_parse_number_int(operand1)) and (operand2 := try_parse_number_int(operand2)):
 
             operand1_str = str(operand1)
             operand2_str = str(operand2)
@@ -52,66 +111,34 @@ def arithmetic_arranger(args, display_answer=False):
                 return 'Error: Numbers cannot be more than four digits.'
 
             # Happy Path
+            # + 2: one space for operand, second white space
             total_space_count = max([operand1_len, operand2_len]) + 2
 
-            operand1_space = str(operand1).rjust(total_space_count, ' ')
-            operand2_space = f'{operator} ' + \
-                str(operand2).rjust(total_space_count - 2, ' ')
-
+            fill = ' '
+            align = '>'
+            operand1_space = f'{operand1:{fill}{align}{total_space_count}}'
+            operand2_space = f'{operator} {operand2:{fill}{align}{total_space_count - 2}}'
             operand1_list.append(operand1_space)
             operand2_list.append(operand2_space)
 
             dashes_list.append('-' * total_space_count)
 
-            result = 0
-            if operator == '+':
-                result = operand1 + operand2
-
-            if operator == '-':
-                result = operand1 - operand2
-
-            result_str = str(result)
-            result_space_count = total_space_count - len(result_str)
-
-            result_space = ' ' * result_space_count
-            result_space += result_str
-
+            result = add_or_substruct_int(operand1, operator, operand2)
+            result_space = f'{result:{fill}{align}{total_space_count}}'
             result_list.append(result_space)
 
         else:
             return 'Error: Numbers must only contain digits.'
 
     final_result = ''
-    final_result += format_result_str(operand1_list)
-    final_result += '\n'
-    final_result += format_result_str(operand2_list)
-    final_result += '\n'
-    final_result += format_result_str(dashes_list)
+    final_result += format_result_str(operand1_list, new_line=True)
+    final_result += format_result_str(operand2_list, new_line=True)
+    final_result += format_result_str(dashes_list, new_line=False)
     if display_answer:
         final_result += '\n'
         final_result += format_result_str(result_list)
 
     return final_result
-
-
-def try_parse_number(str_number):
-    try:
-        number = int(str_number)
-        return number
-    except:
-        return None
-
-
-def format_result_str(item_list):
-    first = True
-    result_str = ''
-    for item in item_list:
-        if first:
-            result_str += item
-            first = False
-        else:
-            result_str += f'    {item}'
-    return result_str
 
 
 if __name__ == '__main__':
